de2cd1e6dbf500f6a096edc4bf23b32de9a49c9a
cpagega <chrispage7@gmail.com>
2026-05-01 15:58:00 -0400

    Moves SDL out of Chip8

--- CDP_Chip8.h
+++ CDP_Chip8.h
@@ -4,64 +4,60 @@
 #include <stdio.h>
 #include <cstring>
 #include <time.h>
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_audio.h>
 
 
-#define FONT_ADDRESS 0x50
-#define SCREEN_WIDTH 64
-#define SCREEN_HEIGHT 32
-#define AUDIO_SAMPLE_SIZE 4000
-#define NO_KEY 0xFF
+namespace CH8
+{
 
-#define SAMPLE_RATE 48000
-#define CHANNELS 2
-#define BYTES_PER_FRAME (sizeof(float) * CHANNELS)
-#define TARGET_MS 30
-#define TARGET_QUEUE_BYTES ((SAMPLE_RATE * TARGET_MS / 1000) * BYTES_PER_FRAME)
+    constexpr auto MEMORY_SIZE = 0x1000;
+    constexpr auto PROGRAM_START_ADDR = 0x200;
+    constexpr auto FONT_ADDR = 0x50;
+    constexpr auto SCREEN_WIDTH = 64;
+    constexpr auto SCREEN_HEIGHT = 32;
+    constexpr auto AUDIO_SAMPLE_SIZE = 4000;
+    constexpr auto NO_KEY = 0xFF;
 
-extern uint8_t font[80];
-extern uint8_t memory[4096];
-extern float audio_buffer[AUDIO_SAMPLE_SIZE];
+    constexpr auto SAMPLE_RATE = 48000;
+    constexpr auto CHANNELS = 2;
+    constexpr auto BYTES_PER_FRAME = (sizeof(float) * CHANNELS);
+    constexpr auto TARGET_MS = 30;
+    constexpr auto TARGET_QUEUE_BYTES = ((SAMPLE_RATE * TARGET_MS / 1000) * BYTES_PER_FRAME);
 
-struct CDP_CHIP8_CPU {
-    bool display;
-    uint16_t PC;
-    uint16_t I;
-    uint16_t stack[16];
-    uint8_t SP;
-    uint8_t delay_timer;
-    uint8_t sound_timer;
-    uint8_t registers[16];
-};
+    extern uint8_t font[80];
+    extern uint8_t memory[MEMORY_SIZE];
+    extern float audio_buffer[AUDIO_SAMPLE_SIZE];
+    extern uint32_t display_buffer[SCREEN_WIDTH * SCREEN_HEIGHT];
 
-struct CDP_CHIP8_Keypad {
-    uint8_t key;
-    uint8_t keys[16];
-    bool keydown;
-};
+    struct CPU {
+        bool display;
+        uint16_t PC;
+        uint16_t I;
+        uint16_t stack[16];
+        uint8_t SP;
+        uint8_t delay_timer;
+        uint8_t sound_timer;
+        uint8_t registers[16];
+    };
 
-struct CDP_CHIP8_GUI {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    SDL_Texture* texture;
-    uint32_t display_buffer[SCREEN_WIDTH * SCREEN_HEIGHT];
-};
-
-struct CDP_CHIP8_SoundOutput {
-    uint16_t tone_hz;
-    uint16_t tone_volume;
-    uint16_t samples_per_second;
-    uint16_t square_wave_period;
-    uint16_t half_square_wave_period;
-    uint32_t wave_position;
-};
+    struct Keypad {
+        uint8_t key;
+        uint8_t keys[16];
+        bool keydown;
+    };
 
 
-void CDP_Chip8_reset(CDP_CHIP8_CPU* cpu, CDP_CHIP8_GUI* gui);
-void CDP_Chip8_load_rom();
-void CDP_Chip8_clear_screen(CDP_CHIP8_GUI* gui);
-void CDP_Chip8_cycle(CDP_CHIP8_CPU* cpu, CDP_CHIP8_GUI* gui, CDP_CHIP8_Keypad* key);
-void CDP_Chip8_render(CDP_CHIP8_GUI* gui);
-uint8_t CDP_Chip8_map_hexkey(int scancode);
-SDL_Window* CDP_Chip8_init_window();
+    struct SoundOutput {
+        uint16_t tone_hz;
+        float    tone_volume;
+        uint16_t samples_per_second;
+        uint16_t square_wave_period;
+        uint16_t half_square_wave_period;
+        uint32_t wave_position;
+    };
+
+    void reset(CPU* cpu);
+    void load_rom();
+    void clear_screen();
+    void cycle(CPU* cpu, Keypad* key);
+    uint8_t map_hexkey(int scancode);
+}
--- main.cpp
+++ main.cpp
@@ -1,18 +1,32 @@
 #include <windows.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_audio.h>
 #include "CDP_Chip8.h"
 
-float audio_buffer[AUDIO_SAMPLE_SIZE];
+float audio_buffer[CH8::AUDIO_SAMPLE_SIZE];
+
+
+
 int main(int argc, char* argv[]) {
-    SDL_Window* window = CDP_Chip8_init_window();
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
+        SDL_Log("SDL could not initialize: %s", SDL_GetError());
+        return 1;
+    }
+    SDL_Window* window = SDL_CreateWindow("CDP-CHIP-8", 1920, 1080, 0);
+    if (!window) {
+        SDL_Log("Window could not be created: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
     SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
     SDL_Texture* sdl_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_XRGB8888, SDL_TEXTUREACCESS_STREAMING, 64, 32);
     SDL_SetTextureScaleMode(sdl_texture, SDL_SCALEMODE_NEAREST);
 
-    CDP_CHIP8_SoundOutput sound = {};
+    CH8::SoundOutput sound = {};
     sound.samples_per_second = 48000;
-    sound.tone_hz = 256;
-    sound.tone_volume = 3000;
+    sound.tone_hz = 350;
+    sound.tone_volume = 5.0f/100;
     sound.square_wave_period = sound.samples_per_second / sound.tone_hz;
     sound.half_square_wave_period = sound.square_wave_period / 2;
 
@@ -45,22 +59,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    CDP_CHIP8_GUI gui = {};
-    gui.window = window;
-    gui.texture = sdl_texture;
-    gui.renderer = renderer;
-
-    CDP_CHIP8_Keypad keypad = {};
-    CDP_CHIP8_CPU cpu = {};
+    CH8::Keypad keypad = {};
+    CH8::CPU cpu = {};
  
-    CDP_Chip8_reset(&cpu, &gui);
+    CH8::reset(&cpu);
     /* High resolution timer for setting clock speed and other timed events */
-    LARGE_INTEGER frequency, last_500Hz, last_60Hz, now;
+    LARGE_INTEGER frequency, last_cycle, last_60Hz, now;
     QueryPerformanceFrequency(&frequency);
-    uint64_t interval_500Hz = frequency.QuadPart / 700;
+    uint64_t clock_cycle = frequency.QuadPart / 700;
     uint64_t interval_60Hz = frequency.QuadPart / 60;
-    QueryPerformanceCounter(&last_500Hz);
-    last_60Hz = last_500Hz;
+    QueryPerformanceCounter(&last_cycle);
+    last_60Hz = last_cycle;
 
     bool running = true;
     while (running) {
@@ -73,8 +82,8 @@ int main(int argc, char* argv[]) {
                 break;
             case(SDL_EVENT_KEY_DOWN):
             {
-                uint8_t key = CDP_Chip8_map_hexkey(event.key.scancode);
-                if (key != NO_KEY) {
+                uint8_t key = CH8::map_hexkey(event.key.scancode);
+                if (key != CH8::NO_KEY) {
                     keypad.key = key;
                     keypad.keys[key] = 1;
                     keypad.keydown = true;
@@ -83,8 +92,8 @@ int main(int argc, char* argv[]) {
             break;
             case(SDL_EVENT_KEY_UP):
             {
-                uint8_t key = CDP_Chip8_map_hexkey(event.key.scancode);
-                if (key != NO_KEY) {
+                uint8_t key = CH8::map_hexkey(event.key.scancode);
+                if (key != CH8::NO_KEY) {
                     keypad.key = key;
                     keypad.keys[key] = 0;
                     keypad.keydown = false;
@@ -95,20 +104,20 @@ int main(int argc, char* argv[]) {
         }
 
         // 60Hz Block (every ~16.67ms)
-        if (now.QuadPart - last_60Hz.QuadPart >= interval_60Hz) {
+        while (now.QuadPart - last_60Hz.QuadPart >= interval_60Hz) {
             if (cpu.delay_timer > 0) cpu.delay_timer--;
             if (cpu.sound_timer > 0) {
                 int queued = SDL_GetAudioStreamQueued(stream);
                 if (queued < 0) {
                     SDL_Log("SDL_GetAudioStreamQueued failed: %s", SDL_GetError());
-                } else if (queued < TARGET_QUEUE_BYTES && cpu.sound_timer > 0) {
-                    for (int32_t sample_index = 0; sample_index < AUDIO_SAMPLE_SIZE - 1; sample_index += 2) {
-                        float sample = ((sound.wave_position++ / sound.half_square_wave_period) % 2) ? 0.25f : -0.25f;
+                } else if (queued < CH8::TARGET_QUEUE_BYTES && cpu.sound_timer > 0) {
+                    for (int32_t sample_index = 0; sample_index < CH8::AUDIO_SAMPLE_SIZE - 1; sample_index += 2) {
+                        float sample = ((sound.wave_position++ / sound.half_square_wave_period) % 2) ? sound.tone_volume : -sound.tone_volume;
                         audio_buffer[sample_index] = sample;
                         audio_buffer[sample_index + 1] = sample;
                     }
 
-                    if (!SDL_PutAudioStreamData(stream, audio_buffer, AUDIO_SAMPLE_SIZE * sizeof(float))) {
+                    if (!SDL_PutAudioStreamData(stream, audio_buffer, CH8::AUDIO_SAMPLE_SIZE * sizeof(float))) {
                         SDL_Log("Error adding audio data to stream\n%s", SDL_GetError());
                         return 1;
                     }
@@ -119,15 +128,18 @@ int main(int argc, char* argv[]) {
                 SDL_PauseAudioStreamDevice(stream);
             }
             cpu.display = true;
-            CDP_Chip8_render(&gui);
-            
+            SDL_UpdateTexture(sdl_texture, NULL, CH8::display_buffer, 256);
+            SDL_RenderTexture(renderer, sdl_texture, NULL, NULL);
+            SDL_RenderPresent(renderer);
             last_60Hz.QuadPart += interval_60Hz;
         }
 
         // 500Hz Block (every ~2ms)
-        if (now.QuadPart - last_500Hz.QuadPart >= interval_500Hz) {
-            CDP_Chip8_cycle(&cpu, &gui, &keypad);
-            last_500Hz.QuadPart += interval_500Hz;
+        int cycles = 0;
+        while (now.QuadPart - last_cycle.QuadPart >= clock_cycle) {
+            CH8::cycle(&cpu, &keypad);
+            last_cycle.QuadPart += clock_cycle;
+            cycles++;
         } 
     }
     SDL_DestroyWindow(window);
